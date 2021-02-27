2e1227e36a6401eb41a09af546d973bd5c8e2c68
柏木優人 <y.kashiwagi@japon-line.co.jp>
2021-02-27 16:45:14 +0900

    gearInchesを追加

--- src/gear.ts
+++ src/gear.ts
@@ -1,13 +1,25 @@
 export default class Gear {
   readonly chainRing: number
   readonly cog: number
+  readonly rim: number
+  readonly tire: number
 
-  constructor (chainRing: number, cog: number) {
+  constructor (chainRing: number, cog: number, rim: number, tire: number) {
     this.chainRing = chainRing
     this.cog = cog
+    this.rim = rim
+    this.tire = tire
   }
 
+  // ギア比
   ratio = (): number => {
     return this.chainRing / this.cog
+  }
+
+  // ギアインチ = 車輪の直径 x ギア比
+  // 車輪の直径 = リムの直径 + (タイヤの厚み x 2)
+  gearInches = () => {
+    const diameter = this.rim + (this.tire * 2)
+    return this.ratio() * diameter
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import Gear from './gear'
 
-const g = new Gear(52, 11)
+// ここが壊れる
+// const g = new Gear(52, 11)
 
-console.log(g.ratio())
+// console.log(g.ratio())
+
+const g = new Gear(52, 11, 26, 1.5)
+console.log(g.gearInches())
