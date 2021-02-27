9416d0c8ee1533990de0999d88ed1ba9286c2afe
YutoKashiwagi <kashiwagi19950714@gmail.com>
2021-02-27 22:41:19 +0900

    Wheelクラスを分離

--- src/gear.ts
+++ src/gear.ts
@@ -1,14 +1,12 @@
-export default class Gear {
+export class Gear {
   readonly chainRing: number
   readonly cog: number
-  readonly rim: number
-  readonly tire: number
+  readonly Wheel: Wheel | null
 
-  constructor (chainRing: number, cog: number, rim: number, tire: number) {
+  constructor (chainRing: number, cog: number, Wheel: Wheel | null = null) {
     this.chainRing = chainRing
     this.cog = cog
-    this.rim = rim
-    this.tire = tire
+    this.Wheel = Wheel
   }
 
   // ギア比
@@ -16,13 +14,32 @@ export default class Gear {
     return this.chainRing / this.cog
   }
 
+  // ギアインチ
+  gearInches = (): number => {
+    if (this.Wheel === null) {
+      throw new Error('Wheelを入力してください')
+    }
+
+    return this.ratio() * this.Wheel.diameter()
+  }
+}
+
+export class Wheel {
+  readonly rim: number
+  readonly tire: number
+
+  constructor (rim: number, tire: number) {
+    this.rim = rim
+    this.tire = tire
+  }
+
   // 車輪の直径
   diameter = (): number => {
     return this.rim + (this.tire * 2)
   }
 
-  // ギアインチ
-  gearInches = (): number => {
-    return this.ratio() * this.diameter()
+  // 車輪の演習
+  circumference = (): number => {
+    return this.diameter() * Math.PI
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import Gear from './gear'
+import { Gear, Wheel } from './gear'
 
-const g = new Gear(52, 11, 26, 1.5)
-console.assert(g.gearInches() === 137.0909090909091)
+const wheel = new Wheel(26, 1.5)
+console.assert(wheel.circumference() === 91.106186954104)
+console.assert(wheel.diameter() === 29)
+
+const gear = new Gear(52, 11, wheel)
+console.assert(gear.gearInches() === 137.0909090909091)
